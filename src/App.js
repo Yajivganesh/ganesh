a54dcd9d2ccaff8fe63189a88ed81b9696ffad55
Yajivganesh <vijayganesh11100@gmail.com>
2022-12-19 19:27:28 +0530

    secondary commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import"./index.css";
 import Home from "./routes/Home";
-import { Route,Routes } from 'react-router-dom';
+// import { Route,Routes } from 'react-router-dom';
 function App() {
   return (
-   <>
-   <Routes>
-   <Route path="/"element={<Home/>}/>
-   </Routes>
-    </>
+    <div className="App">
+      <Home/>
+      
+    </div>
   )
 }
 
